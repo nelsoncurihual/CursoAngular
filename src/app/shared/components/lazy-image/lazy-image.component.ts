6ae96b73b2cd5c1a6952b6a1db8837aa29521575
Nelson Curihual <n.curihual01@ufromail.cl>
2023-08-06 22:35:18 -0700

    se termina el proyecto

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+})
+export class LazyImageComponent implements OnInit {
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  @Input()
+  public width: string = '';
+
+  @Input()
+  public height: string = '';
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('Url property is required');
+  }
+
+  onLoad():void {
+    console.log('Image loaded');
+    this.hasLoaded = true;
+  }
+}
